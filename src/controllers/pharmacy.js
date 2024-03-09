a2956e3728fe0ae9394a1d069ef68b3d03ad558d
Abigail Addo <abigailadwoaaddo@gmail.com>
2024-03-09 14:40:14 +0000

    Update

--- src/controllers/pharmacy.js
+++ src/controllers/pharmacy.js
@@ -79,19 +79,18 @@ export const getDrugById = async (req, res) => {
 // update an item by id
 export const updateDrugById = async (req, res) => {
   try {
-
-    
-    let drug = await Pharmacy.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
     const existingDrug = await Pharmacy.findOne({
       drug_code: req.body.drug_code,
-      _id: { $ne: req.params.id } // Exclude the current drug from the check
+      _id: { $ne: req.params.id }, // Exclude the current drug from the check
     });
 
     if (existingDrug) {
       return res.status(400).json({ message: "Drug code already exists" });
     }
+
+    let drug = await Pharmacy.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
     if (!drug) {
       return res.status(404).json({ message: "Drug does not exist" });
